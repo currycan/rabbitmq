36df67ad37016abccef0301108965275ed86056c
currycan <ansandy@foxmail.com>
2018-07-24 10:10:27 +0800

    Update cluster_entrypoint.sh

--- source/rabbitmq/cluster_entrypoint.sh
+++ source/rabbitmq/cluster_entrypoint.sh
@@ -5,7 +5,7 @@ if [ 0"$RABBITMQ_NODE_TYPE" != "0" ];then
     if [ -e /var/lib/rabbitmq/mnesia/*.pid ]; then
         exec "$@"
     else
-        if [ $RABBITMQ_NODE_TYPE == "disc" ];then
+        if [ $RABBITMQ_NODE_TYPE == "stats" ];then
             exec "$@"
         elif [ $RABBITMQ_NODE_TYPE == "ram" ];then
             docker-entrypoint.sh rabbitmq-server -detached # 先按官方入口文件启动且是后台运行
@@ -13,8 +13,14 @@ if [ 0"$RABBITMQ_NODE_TYPE" != "0" ];then
             rabbitmqctl join_cluster --ram $RABBITMQ_CLUSTER_NODE_NAME # 加入rmqha_node0集群
             rabbitmqctl start_app # 启动应用
             rabbitmqctl stop # 停止所有服务
+        elif [ $RABBITMQ_NODE_TYPE == "disc" ];then
+            docker-entrypoint.sh rabbitmq-server -detached # 先按官方入口文件启动且是后台运行
+            rabbitmqctl stop_app # 停止应用
+            rabbitmqctl join_cluster --disc $RABBITMQ_CLUSTER_NODE_NAME # 加入rmqha_node0集群
+            rabbitmqctl start_app # 启动应用
+            rabbitmqctl stop # 停止所有服务
         else
-            echo "[Error] please check the RABBITMQ_NODE_TYPE, it should be 'ram' or 'disc'!"
+            echo "[Error] please check the RABBITMQ_NODE_TYPE, it should be 'ram', 'disc' or 'stats'!"
         fi
         sleep 1s
         exec "$@"
