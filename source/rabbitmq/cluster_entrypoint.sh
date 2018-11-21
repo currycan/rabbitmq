573d39a40e757442f1d6a36156b527fe8c3c3949
currycan <ansandy@foxmail.com>
2018-11-21 09:16:39 +0800

    Update cluster_entrypoint.sh

--- source/rabbitmq/cluster_entrypoint.sh
+++ source/rabbitmq/cluster_entrypoint.sh
@@ -7,7 +7,6 @@ if [ 0"$RABBITMQ_NODE_TYPE" != "0" ];then
     else
         if [ $RABBITMQ_NODE_TYPE == "stats" ];then
             touch /var/lib/rabbitmq/is_not_first_time
-            rabbitmq-plugins enable --offline rabbitmq_delayed_message_exchange
             exec "$@"
         elif [ $RABBITMQ_NODE_TYPE == "ram" ];then
             docker-entrypoint.sh rabbitmq-server -detached # 先按官方入口文件启动且是后台运行
@@ -18,7 +17,6 @@ if [ 0"$RABBITMQ_NODE_TYPE" != "0" ];then
             rabbitmqctl start_app # 启动应用
             rabbitmqctl stop # 停止所有服务
         elif [ $RABBITMQ_NODE_TYPE == "disc" ];then
-            rabbitmq-plugins enable --offline rabbitmq_delayed_message_exchange
             docker-entrypoint.sh rabbitmq-server -detached # 先按官方入口文件启动且是后台运行
             rabbitmqctl stop_app # 停止应用
             rabbitmqctl join_cluster --disc $RABBITMQ_CLUSTER_NODE_NAME # 加入rmqha_node0集群
